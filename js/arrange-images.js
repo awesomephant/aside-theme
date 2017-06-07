111900262d0aa66fed432ea1b756f51fb961da15
Max Koehler <max@koehler-kn.de>
2017-06-07 11:36:09 +0100

    redesgin landing page

--- js/arrange-images.js
+++ js/arrange-images.js
@@ -2,12 +2,32 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-var images = document.querySelectorAll('.home-images img');
-
-for (var i = 0; i < images.length; i++) {
-    let image = images[i];
-    let w = getRandomInt(25, 40);
-    image.style.width = w + '%';
-    image.style.left = getRandomInt(-10, (100 - w)) + '%';
-    image.style.top = getRandomInt(-20, 20) + '%';
+function shuffle(a) {
+    for (let i = a.length; i; i--) {
+        let j = Math.floor(Math.random() * i);
+        [a[i - 1], a[j]] = [a[j], a[i - 1]];
+    }
 }
+
+const imageCount = 4;
+const gridSize = 10;
+const imageUrls = ['SIP-4728.jpg', 'SIP-4731.jpg', 'SIP-4738.jpg', 'SIP-4740.jpg', 'SIP-4746.jpg', 'SIP-4814.jpg', 'SIP-4817.jpg', 'wall.jpg'];
+var imageContainer = document.getElementById('js-imageContainer');
+
+var spawnImages = function () {
+    shuffle(imageUrls);
+    for (var i = 0; i < imageCount; i++) {
+        imageUrls[imageCount - 1] = 'green.jpg';
+        let imageUrl = imageUrls[i];
+        let w = getRandomInt(45, 70);
+        let imgEl = document.createElement('img');
+        imgEl.setAttribute('src', themeLink + '/assets/source/' + imageUrl)
+        imgEl.style.width = w + '%';
+        imgEl.style.left = (getRandomInt(-1, 5) * gridSize) + '%';
+        imgEl.style.top = (getRandomInt(-1, 4) * gridSize) + '%';
+
+        imageContainer.appendChild(imgEl)
+    }
+}
+
+spawnImages()
